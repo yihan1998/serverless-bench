3019e41a71a5929bf206a7e6eb93a94958fd2ff2
yihan <yangyihan1998@gmail.com>
2024-02-07 14:36:42 +0800

    update

--- grpc-echo/server/server.go
+++ grpc-echo/server/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"time"
 
 	echo "github.com/yihan1998/serverless-bench/grpc-echo/proto"
 	"google.golang.org/grpc"
@@ -13,16 +17,50 @@ var port = 8080
 type echoServer struct {
 }
 
-func main () {
+func (e *echoServer) Echo(ctx context.Context, req *echo.Request) (*echo.Response, error) {
+	log.Printf("Received: %s", req)
+	return &echo.Response{Msg: fmt.Sprintf("%s - pong", req.Msg)}, nil
+}
+
+func (e *echoServer) EchoStream(stream echo.EchoService_EchoStreamServer) error {
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			fmt.Println("Client disconnected")
+			return nil
+		}
+
+		if err != nil {
+			fmt.Println("Failed to receive ping")
+			return err
+		}
+
+		fmt.Printf("Replying to ping %s at %s\n", req.Msg, time.Now())
+
+		err = stream.Send(&ping.Response{
+			Msg: fmt.Sprintf("pong %s", time.Now()),
+		})
+
+		if err != nil {
+			fmt.Printf("Failed to send pong %s\n", err)
+			return err
+		}
+	}
+}
+
+func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	pingServer := &pingServer{}
+	echoServer := &echoServer{}
 
 	grpcServer := grpc.NewServer()
-	reflection.Register(grpcServer)
-	ping.RegisterPingServiceServer(grpcServer, pingServer)
-	grpcServer.Serve(lis)
+	echo.RegisterEchoServiceServer(grpcServer, echoServer)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
 }
