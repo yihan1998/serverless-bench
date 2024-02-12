5646a07f7c6ff9df64300b308894e9ec07489dbc
yihan <yangyihan1998@gmail.com>
2024-02-12 14:56:08 +0800

    test?

--- cmd/loader.go
+++ cmd/loader.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"github.com/yihan1998/serverless-bench/pkg/driver"
+	"regexp"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	urlRegex = regexp.MustCompile("at URL:\nhttp://([^\n]+)")
 )
 
 func main() {
-	driver.NewDriver()
-	// driver.deployKnative()
+	log.Infof("Test")
 }
