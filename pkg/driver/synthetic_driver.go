fe5b94a1ca615b870a29ae6ff56dbded0e70f869
yihan <yangyihan1998@gmail.com>
2024-02-18 11:55:44 +0800

    Add driver package

---
+++ pkg/driver/synthetic_driver.go
@@ -0,0 +1,183 @@
+package driver
+
+import (
+	"sync"
+	"sync/atomic"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/vhive-serverless/loader/pkg/common"
+	"github.com/vhive-serverless/loader/pkg/config"
+	"github.com/vhive-serverless/loader/pkg/generator"
+	"github.com/vhive-serverless/loader/pkg/trace"
+)
+
+type DriverConfiguration struct {
+	LoaderConfiguration *config.LoaderConfiguration
+	IATDistribution     common.IatDistribution
+	ShiftIAT            bool // shift the invocations inside minute
+	TraceGranularity    common.TraceGranularity
+	TraceDuration       int // in minutes
+
+	YAMLPath string
+	TestMode bool
+
+	Functions []*common.Function
+}
+
+type Driver struct {
+	Configuration          *DriverConfiguration
+	SpecificationGenerator *generator.SpecificationGenerator
+}
+
+func NewDriver(driverConfig *DriverConfiguration) *Driver {
+	return &Driver{
+		Configuration:          driverConfig,
+		SpecificationGenerator: generator.NewSpecificationGenerator(driverConfig.LoaderConfiguration.Seed),
+	}
+}
+
+func (c *DriverConfiguration) WithWarmup() bool {
+	if c.LoaderConfiguration.WarmupDuration > 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (d *Driver) invokeFunction(metadata *InvocationMetadata) {
+	defer metadata.AnnounceDoneWG.Done()
+
+	var success bool
+
+	var record *mc.ExecutionRecord
+	switch d.Configuration.LoaderConfiguration.Platform {
+	case "Knative":
+		success, record = InvokeGRPC(
+			metadata.Function,
+			metadata.RuntimeSpecifications,
+			d.Configuration.LoaderConfiguration,
+		)
+	case "OpenWhisk":
+		success, record = InvokeOpenWhisk(
+			metadata.Function,
+			metadata.RuntimeSpecifications,
+			metadata.AnnounceDoneExe,
+			metadata.ReadOpenWhiskMetadata,
+		)
+	case "AWSLambda":
+		success, record = InvokeAWSLambda(
+			metadata.Function,
+			metadata.RuntimeSpecifications,
+			metadata.AnnounceDoneExe,
+		)
+	case "Dirigent":
+		success, record = InvokeDirigent(
+			metadata.Function,
+			metadata.RuntimeSpecifications,
+			d.Configuration.LoaderConfiguration,
+		)
+	default:
+		log.Fatal("Unsupported platform.")
+	}
+
+	record.Phase = int(metadata.Phase)
+	record.InvocationID = composeInvocationID(d.Configuration.TraceGranularity, metadata.MinuteIndex, metadata.InvocationIndex)
+
+	metadata.RecordOutputChannel <- record
+
+	if success {
+		atomic.AddInt64(metadata.SuccessCount, 1)
+	} else {
+		atomic.AddInt64(metadata.FailedCount, 1)
+		atomic.AddInt64(&metadata.FailedCountByMinute[metadata.MinuteIndex], 1)
+	}
+}
+
+func (d *Driver) individualFunctionDriver(function *common.Function, announceFunctionDone *sync.WaitGroup,
+	addInvocationsToGroup *sync.WaitGroup, readOpenWhiskMetadata *sync.Mutex, totalSuccessful *int64,
+	totalFailed *int64, totalIssued *int64, recordOutputChannel chan interface{}) {
+
+}
+
+func (d *Driver) internalRun(iatOnly bool) {
+	var successfulInvocations int64
+	var failedInvocations int64
+	var invocationsIssued int64
+
+	readOpenWhiskMetadata := sync.Mutex{}
+	allFunctionsInvoked := sync.WaitGroup{}
+	allIndividualDriversCompleted := sync.WaitGroup{}
+	allRecordsWritten := sync.WaitGroup{}
+	allRecordsWritten.Add(1)
+
+	backgroundProcessesInitializationBarrier, globalMetricsCollector, totalIssuedChannel, scraperFinishCh := d.startBackgroundProcesses(&allRecordsWritten)
+
+	if !iatOnly {
+		log.Info("Generating IAT and runtime specifications for all the functions")
+		for i, function := range d.Configuration.Functions {
+			spec := d.SpecificationGenerator.GenerateInvocationData(
+				function,
+				d.Configuration.IATDistribution,
+				d.Configuration.ShiftIAT,
+				d.Configuration.TraceGranularity,
+			)
+
+			d.Configuration.Functions[i].Specification = spec
+		}
+	}
+
+	backgroundProcessesInitializationBarrier.Wait()
+
+	log.Infof("Starting function invocation driver\n")
+	for _, function := range d.Configuration.Functions {
+		allIndividualDriversCompleted.Add(1)
+
+		go d.individualFunctionDriver(
+			function,
+			&allIndividualDriversCompleted,
+			&allFunctionsInvoked,
+			&readOpenWhiskMetadata,
+			&successfulInvocations,
+			&failedInvocations,
+			&invocationsIssued,
+			globalMetricsCollector,
+		)
+	}
+
+	allIndividualDriversCompleted.Wait()
+	if atomic.LoadInt64(&successfulInvocations)+atomic.LoadInt64(&failedInvocations) != 0 {
+		log.Debugf("Waiting for all the invocations record to be written.\n")
+
+		totalIssuedChannel <- atomic.LoadInt64(&invocationsIssued)
+		scraperFinishCh <- 0 // Ask the scraper to finish metrics collection
+
+		allRecordsWritten.Wait()
+	}
+
+	log.Infof("Trace has finished executing function invocation driver\n")
+	log.Infof("Number of successful invocations: \t%d\n", atomic.LoadInt64(&successfulInvocations))
+	log.Infof("Number of failed invocations: \t%d\n", atomic.LoadInt64(&failedInvocations))
+}
+
+func (d *Driver) RunExperiment(iatOnly bool) {
+	if d.Configuration.WithWarmup() {
+		trace.DoStaticTraceProfiling(d.Configuration.Functions)
+	}
+
+	trace.ApplyResourceLimits(d.Configuration.Functions, d.Configuration.LoaderConfiguration.CPULimit)
+
+	switch d.Configuration.LoaderConfiguration.Platform {
+	case "Knative":
+		DeployFunctions(d.Configuration.Functions,
+			d.Configuration.YAMLPath,
+			d.Configuration.LoaderConfiguration.IsPartiallyPanic,
+			d.Configuration.LoaderConfiguration.EndpointPort,
+			d.Configuration.LoaderConfiguration.AutoscalingMetric)
+	default:
+		log.Fatal("Unsupported platform.")
+	}
+
+	// Generate load
+	d.internalRun(iatOnly)
+}
