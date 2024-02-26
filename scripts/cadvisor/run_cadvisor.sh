ec04df243215027a473cecf5c951e7a7c3eaf4e0
yihan <yangyihan1998@gmail.com>
2024-02-26 21:22:29 +0800

    update script

---
+++ scripts/cadvisor/run_cadvisor.sh
@@ -0,0 +1,13 @@
+VERSION=v0.46.0
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --volume=/dev/disk/:/dev/disk:ro \
+  --publish=8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  --privileged \
+  --device=/dev/kmsg \
+  gcr.io/cadvisor/cadvisor:$VERSION
